88576add50f03c47b2e5534a7ad79d18e32bf98b
Anthony Okpako <melowsworld@yahoo.co.uk>
2022-02-02 19:31:17 +0100

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Tony on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
